f589f136aa7673826e0790ca0d25386b24fe1888
walidemars <walidemars52@gmail.com>
2024-09-26 12:03:01 +0500

    feauture\task1293

---
+++ src/timus/task1293.java
@@ -0,0 +1,20 @@
+package timus;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class task1293 {
+    public task1293() {
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int n = in.nextInt();
+        int result = a * b * n * 2;
+        out.println(result);
+        out.flush();
+    }
+}
